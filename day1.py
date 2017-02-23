279458f3e917c2f4d34124c4d22219aade9048b7
BorjikhanLeo <kevin_thu@163.com>
2017-02-24 01:39:59 +0800

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,35 @@
+'''
+This program works will in Ipython
+Learn from BarbaGroup
+Author Leo
+'''
+
+# Assigning module
+import numpy as np
+import matplotlib.pyplot as plt
+import time, sys
+
+# Variable Decalaration
+nx = 41
+dx = 2 / (nx - 1)
+nt = 25
+dt = 0.025
+c  = 1
+
+# Assigning Initial Field
+u = np.ones(nx)
+u[int(0.5 / dx):int(1 / dx + 1)] = 2
+
+# initialize figure
+plt.figure(figsize=(10,10), dpi=100)
+plt.plot(np.linspace(0, 2, nx), u)
+
+# calculation begin
+un = np.ones(nx)    # temp array
+for n in range(nt): #loop for values of n from 0 to nt, so it will run nt times
+    un = u.copy()
+    for i in range(1, nx):
+        u[i] = un[i] - c * dt / dx *(un[i] - un[i - 1])
+
+plt.plot(np.linspace(0, 2, nx), u)
+plt.show()
